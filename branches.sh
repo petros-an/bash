e1f8fa0e307afea8843eab51b3fdcd10ad1ea42f
Petros Anagnou <petros.anagnou@complyadvantage.com>
2019-11-05 20:36:24 +0000

    README

--- branches.sh
+++
@@ -1,5 +0,0 @@
-x=`git for-each-ref --format='%(refname:short)' refs/heads | xargs echo`
-y=$(git branch | awk '/^\* / { print $2 }')
-z=`~/options/bin/options $y $x | tr -cd "[:print:]\n" `
-git checkout $z
-
